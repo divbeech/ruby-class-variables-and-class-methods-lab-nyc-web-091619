ef27aa1c9ab26a7b49282b0fa96f62e8eff3d2a5
Divya Beech <dyadav1006@gmail.com>
2019-09-29 21:29:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ def self.artist_count
 artist_count = {}
   @@artists.each do |artist|
     if artist_count[artist]
-      artist_count[artist] = 1
+      artist_count[artist] += 1
     else
       artist_count[artist] =1
     end
@@ -43,7 +43,7 @@ def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
     if genre_count[genre]
-     genre_count[genre] = 1
+     genre_count[genre] += 1
     else
       genre_count[genre] =1
     end
